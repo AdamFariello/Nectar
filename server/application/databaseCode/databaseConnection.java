818c5c8ce985fdc0b94ed71c1c3f8b2e062f4be9
AdamFariello <ajf270@scarletmail.rutgers.edu>
2023-04-11 14:29:00 -0400

    Finish databaseConnection.java

---
+++ server/application/databaseCode/databaseConnection.java
@@ -0,0 +1,69 @@
+package databaseCode;
+
+import java.sql.*;
+
+public class databaseConnection{
+	private String server   = "Nectar";
+	private String timeZone = "EDT";
+	private static String username, password, url;
+	private static Connection connection;
+	
+	public databaseConnection() {
+		//Default connection 
+		//Login is from server/database/.notes
+		username = "Aeyjaejay";
+		password = "1234";
+		url 	 = "jdbc:mysql://localhost:3306/" + server
+				 + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=" + timeZone
+		;
+	}
+	@SuppressWarnings("static-access")
+	public databaseConnection(String username, String password) {
+		this.username = username;
+		this.password = password;
+		url 	 = "jdbc:mysql://localhost:3306/" + server
+				 + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=" + timeZone
+		;
+	}
+	@SuppressWarnings("static-access")
+	public databaseConnection(String username, String password, String url) {
+		this.username = username;
+		this.password = password;
+		this.url 	  = url;
+	}
+		
+	public static Connection Connection() {
+		try	{
+			return connection = DriverManager.getConnection(url,username,password);
+		}	catch(Exception e)	{
+			System.out.println(e);
+		}
+		
+		return null;
+	}
+	public void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e1) {
+			System.out.println(e1);
+		} catch (Exception e2) {
+			System.out.println(e2);
+		}
+	}
+	
+	public String toString() {
+		String string = null;
+		try {
+			string = 
+				  "Url: " 		 +url		 +"\n"
+				+ "UserName: "	 +username	 +"\n"
+				+ "Password: "	 +password	 +"\n"
+				+ "Connection: " +connection +"\n"
+			;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return string;
+	}
+}

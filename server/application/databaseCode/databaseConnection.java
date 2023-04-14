fdf3f5f90e9fa573cee97aa2e4b6a0b746dfe098
AdamFariello <ajf270@scarletmail.rutgers.edu>
2023-04-13 20:37:25 -0400

    Added init function to connection file

--- server/application/databaseCode/databaseConnection.java
+++ server/application/databaseCode/databaseConnection.java
@@ -11,20 +11,13 @@ public class databaseConnection{
 	//Connections
 	public databaseConnection() {
 		//Default connection 
-		//Login is from server/database/.notes
 		username = "Yocif";
-		password = "1234";
-		url 	 = "jdbc:mysql://localhost:3306/" + server
-				 + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=" + timeZone
-		;
+		password = "1234";		
 	}
 	@SuppressWarnings("static-access")
 	public databaseConnection(String username, String password) {
 		this.username = username;
 		this.password = password;
-		url 	 = "jdbc:mysql://localhost:3306/" + server
-				 + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=" + timeZone
-		;
 	}
 	@SuppressWarnings("static-access")
 	public databaseConnection(String username, String password, String url) {
@@ -32,25 +25,38 @@ public class databaseConnection{
 		this.password = password;
 		this.url 	  = url;
 	}
+	@SuppressWarnings("deprecation")
+	public void init () {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			url = "jdbc:mysql://localhost:3306/" + server
+				+ "?useUnicode=true&useJDBCCompliant"
+				+ "TimezoneShift=true&useLegacyDatetimeCode=false"
+				+ "&serverTimezone=" + timeZone
+			;
+		}
+	}
 	
 	
 	//Connections setups
+	@SuppressWarnings("finally")
 	public static Connection Connection() {
 		try	{
 			return connection = DriverManager.getConnection(url,username,password);
 		} catch (Exception e) {
 			System.out.println(e);
+		} finally {
+			return null;	
 		}
-		
-		return null;
 	}
 	public void closeConnection() {
 		try {
 			connection.close();
-		} catch (SQLException e1) {
-			System.out.println(e1);
-		} catch (Exception e2) {
-			System.out.println(e2);
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 	}
 	
--- server/application/test/test.java
+++ server/application/test/test.java
@@ -1,5 +1,20 @@
 package test;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import databaseCode.*;
 
 public class test {
-
+	public static void main (String args[]) throws SQLException {
+		databaseConnection dc = new databaseConnection();
+		dc.Connection();
+		databaseQueries dq = new databaseQueries(dc);
+		
+		ResultSet rs = dq.describeTable("user");
+		rs.next();
+		for (int i = 1; true; i++) 
+			System.out.println(rs.getString(i));
+		
+		
+	}
 }

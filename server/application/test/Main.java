c6bcd1052146ec8065cc3edc7e2859f1554a5ca6
AdamFariello <ajf270@scarletmail.rutgers.edu>
2023-04-21 05:24:45 -0400

    Don't use these characters: ':', '-', '+' -- anywhere in your java project

---
+++ server/application/test/Main.java
@@ -0,0 +1,23 @@
+package test;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import databaseCode.*;
+
+public class Main {
+	public static void main (String args[]) throws SQLException {		
+		databaseConnection dc = new databaseConnection();
+		String table = "user";
+		dc.startConnection(table);
+		databaseQueries dq = new databaseQueries(dc);
+		
+		ArrayList<String> tableInputs = new ArrayList<String>();
+		tableInputs.add("0");
+		tableInputs.add("KbToys@gmail.com");
+		tableInputs.add("PrintYourOwnMoney");
+		tableInputs.add("111-222-3390");
+		
+		dq.insertIntoTable_WithPrimaryKey(table, tableInputs);
+	}
+}

2168325e7a742f7a73cf033833b28375151dd284
AdamFariello <ajf270@scarletmail.rutgers.edu>
2023-04-28 20:24:33 -0400

    Commit before fixing lib

--- server/application/databaseCode/DBRetrieve.java
+++ server/application/databaseCode/DBRetrieve.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 
 public class DBRetrieve extends DBConversions {	
+	
 	//Columns and DataTypes
-	/*
 	public static ArrayList<Object> getColumnsFromTable_ArrObj (String table) {
 		return convertColumn_RStoArrObj(getColumnsFromTable_RS(table));
 	}
@@ -33,7 +33,6 @@ public class DBRetrieve extends DBConversions {
 		} 
 		return null;
 	}
-	*/
 	
 	public static ArrayList<Object> getDatatypesOfTable_ArrObj (String table) {
 		return convertColumn_2DRStoArrObj(getColumnsAndDatatypeFromTable_RS(table), 2);
@@ -41,26 +40,7 @@ public class DBRetrieve extends DBConversions {
 	public static ArrayList<String> getDatatypesOfTable_ArrStr (String table) {
 		return convertColumn_2DRStoArrStr(getColumnsAndDatatypeFromTable_RS(table), 2);
 	}
-	public static ResultSet getDatatypesFromTable_RS (String table) {
-		//TODO: Create error to catch quotation marks missing when inserting into query string
-		try { 						
-			//Example: Getting all data types names from a table
-			//	SELECT COLUMN_NAME, DATA_TYPE
-			//	FROM information_schema.COLUMNS 
-			//	WHERE table_schema='nectarDB_user' 
-			//		and TABLE_NAME='user';
-			String query = "SELECT DATA_TYPE " 
-						 + "FROM INFORMATION_SCHEMA.COLUMNS "
-						 + "WHERE TABLE_SCHEMA = ? AND TABLE_NAME= ?";
-			PreparedStatement ps = dbConnetion.getConnection().prepareStatement(query);
-			ps.setString(1, dbConnetion.getCurrentServer());
-			ps.setString(2, table);
-			return ps.executeQuery();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		return null;
-	}
+
 		
 	/*
 	public static ArrayList< ArrayList<Object> > getColumnsAndDatatypeFromTable_2DArrObj (String table) {

3617b8c4ce1c6df8d7889993b1e53510fafead63
roodlandy <78751319+roodlandy@users.noreply.github.com>
2023-05-01 17:40:06 -0400

    websocket server completed

--- client/gui/controller/login-page.js
+++ client/gui/controller/login-page.js
@@ -6,10 +6,15 @@ loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    if (username === "user" && password === "pass") {
+    if (username === "user" && password === "pass") {fl
         alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+window.onload = function() {
+    alert("You started up");
+    ws = new WebSocket("ws://localhost:8080/events/");
+}
--- client/gui/model/socketclient.js
+++
@@ -1,4 +0,0 @@
-var net = require('net');
-var client = net.connect(8999, 'localhost');
-client.write('Hello from node.js');
-client.end();

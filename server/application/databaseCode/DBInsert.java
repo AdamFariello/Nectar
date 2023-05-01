51348f7efad5e71170f9c2b158a01d3c8e1e058f
AdamFariello <ajf270@scarletmail.rutgers.edu>
2023-05-01 18:08:52 -0400

    Adding ability to add multiple rows

--- server/application/databaseCode/DBInsert.java
+++ server/application/databaseCode/DBInsert.java
@@ -4,25 +4,49 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class DBInsert extends DBRetrieve {			
-	//TODO
-	//	1) Check for incorrect inputs for columns
-	//	2) java.sql.SQLException: Column count doesn't match value count at row 1
-	//	   (Insert error catch to stop this)
-	//	3) Account for no primary key when inserting into table
+	//TODO: Check for incorrect inputs for columns
+	//TODO: java.sql.SQLException: Column count doesn't match value 
+	//		count at row 1 
+	//		(Insert error catch to stop this)
+	//TODO: Account for no primary key when inserting into table
+	//TODO: Check for error of using primary key method and not giving a primary key
+	//		(Possible un-needed since I already check for incorrect lengths...)
+	
 	
 	public static boolean insertIntoTableWithPrimaryKey
 	(String table, ArrayList<Object> tableInputs) {					
+		return insertIntoTableWithPrimaryKey(table, tableInputs);
+	}
+	public static boolean insertIntoTableWithPrimaryKey 
+	(String table, ArrayList<ArrayList<Object>> tableInputs) {					
+		ArrayList<String> columnsOfTable   = getColumnsOfTable_ArrStr(table);
+		ArrayList<String> datatypesOfTable = getDatatypesOfTable_ArrStr(table);
+		
+		
+		String queryColumns = columnsOfTable.toString()
+											.replace("[", "")
+											.replace("]", "");
+		
 		//Create "?" for each given argument, and remove final ", " in string
 		String queryInserts = "?, ".repeat(tableInputs.size());
 		queryInserts 	    = queryInserts.substring(0, queryInserts.length() - 2);
-		String queryformat = "INSERT INTO %s VALUES (%s)";
-		String query 	   = String.format(queryformat, table, queryInserts);
+				
+		String queryValues = "";
+		for (ArrayList<Object> ArrObj: tableInputs) {
+			queryValues += ArrObj.toString()
+								 .replace("[", "(")
+								 .replace("]", ")");
+			queryValues += ", ";
+		}
+		queryValues = queryValues.substring(0, queryValues.length()-2);
 		
-		return insertIntoTable(
-			query, tableInputs, 
-			getColumnsOfTable_ArrStr(table), getDatatypesOfTable_ArrStr(table)
-		);
+		String queryFormat = "INSERT INTO %s (%s) VALUES %s";
+		String query = String.format(queryFormat, table, queryColumns, queryInserts);
+		
+		return insertIntoTable (query, tableInputs, columnsOfTable, datatypesOfTable);
 	}
+	
+
 	public static boolean insertIntoTableWithOutPrimaryKey
 	(String table, ArrayList<Object> tableInputs) {
 		ArrayList<String> columnsOfTable = getColumnsOfTable_ArrStr(table);
@@ -30,15 +54,45 @@ public class DBInsert extends DBRetrieve {
 		ArrayList<String> datatypesOfTable = getDatatypesOfTable_ArrStr(table);
 		datatypesOfTable.remove(0);
 		
-		//Create "?" for each given argument, and remove final ", " in string
-		String queryInserts = "?, ".repeat(tableInputs.size()); 
-		queryInserts 	    = queryInserts.substring(0, queryInserts.length()-2);
 		String queryColumns = columnsOfTable.toString()
 											.replace("[", "")
 											.replace("]", "");
-		String queryString  = "INSERT INTO %s (%s) VALUES %s";
-		String query = String.format(queryString, table, queryInserts, queryColumns);
 		
+		//Create "?" for each given argument, and remove final ", " in string
+		String queryInserts = "?, ".repeat(tableInputs.size()); 
+		queryInserts 	    = queryInserts.substring(0, queryInserts.length()-2);
+		
+		String queryFormat = "INSERT INTO %s (%s) VALUES (%s)";
+		String query 	   = String.format(queryFormat, table, queryColumns, queryInserts);
+		return insertIntoTable (query, tableInputs, columnsOfTable, datatypesOfTable);
+	}
+	public static boolean insertIntoTableWithOutPrimaryKey
+	(String table, ArrayList<ArrayList<Object>> tableInputs) {
+		ArrayList<String> columnsOfTable = getColumnsOfTable_ArrStr(table);
+		columnsOfTable.remove(0);
+		ArrayList<String> datatypesOfTable = getDatatypesOfTable_ArrStr(table);
+		datatypesOfTable.remove(0);
+		
+
+		String queryColumns = columnsOfTable.toString()
+											.replace("[", "")
+											.replace("]", "");
+		
+		//Create "?" for each given argument, and remove final ", " in string
+		String queryInserts = "?, ".repeat(tableInputs.size()); 
+		queryInserts 	    = queryInserts.substring(0, queryInserts.length()-2);
+		
+		String queryValues = "";
+		for (ArrayList<Object> ArrObj: tableInputs) {
+			queryValues += ArrObj.toString()
+								 .replace("[", "(")
+								 .replace("]", ")");
+			queryValues += ", ";
+		}
+		queryValues = queryValues.substring(0, queryValues.length()-2);
+		
+		String queryFormat = "INSERT INTO %s (%s) VALUES (%s)";
+		String query 	   = String.format(queryFormat, table, queryColumns, queryInserts);
 		return insertIntoTable (query, tableInputs, columnsOfTable, datatypesOfTable);
 	}
 	
@@ -50,18 +104,19 @@ public class DBInsert extends DBRetrieve {
 			
 			for (int i = 1; !columnsOfTable.isEmpty(); i++) {
 				//Either i - 1, or all else is i + 1
-				Object tableInputObj = tableInputs.get(i - 1); 
-				String rowCurCol	  = columnsOfTable.get(0);
-				String rowCurDataType = datatypesOfTable.get(1);
+				//Object tableInputObj  = tableInputs.get(i - 1); 
+				Object tableInputObj   = tableInputs.remove(0); 
+				String currRowCol	   = columnsOfTable.remove(0);
+				String currRowDataType = datatypesOfTable.remove(0);
 				
 				//Can't be a switch since switches don't allow functions
-				if (rowCurDataType.equals("varchar")    	 && tableInputObj instanceof String) {
+				if (currRowDataType.contains("varchar")    	  && tableInputObj instanceof String) {
 					ps.setString(i, (String) tableInputObj);
-				} else if (rowCurDataType.equals("int")      && tableInputObj instanceof Integer) {
+				} else if (currRowDataType.equals("int")      && tableInputObj instanceof Integer) {
 					ps.setInt(i, (Integer) tableInputObj);
-				} else if (rowCurDataType.equals("double")   && tableInputObj instanceof Double) {
+				} else if (currRowDataType.equals("double")   && tableInputObj instanceof Double) {
 					ps.setDouble(i, (Double) tableInputObj);
-				} else if (rowCurDataType.equals("datetime") && 
+				} else if (currRowDataType.equals("datetime") && 
 						  (tableInputObj instanceof java.util.Date || tableInputObj instanceof java.sql.Date)) { 
 					//TODO: Check if it includes time
 					ps.setDate(i, (java.sql.Date) tableInputObj); //Format: "MM/DD/YYYY"
@@ -76,7 +131,7 @@ public class DBInsert extends DBRetrieve {
 					
 					throw new errorIncorrectDataTypeForTheTable(
 							tableInputString, tableInputDataTypeString, 
-							rowCurCol, rowCurDataType
+							currRowCol, currRowDataType
 					);
 				}
 			}
--- server/application/databaseCode/DBTest.java
+++ server/application/databaseCode/DBTest.java
@@ -20,99 +20,14 @@ public class DBTest {
 		con.startConnection(db);		
 		DBQuery test = new DBQuery(con);
 		
-		/*
-		//Testing db_retrieve
-		ArrayList<String> tableColumns = new ArrayList<String>();
-		tableColumns.add("user_email");
-		tableColumns.add("user_password");
-		ArrayList< ArrayList<String> > list = test.getSomeFromTable_2DArrStr(table, tableColumns);
-		System.out.println(list);
-		*/
 		
-		/*
-		//Testing Insertions with primary key
+		//Testing insertIntoTableWithOutPrimaryKey
 		ArrayList<Object> tableInputs = new ArrayList<Object>();
-		tableInputs.add(0);
-		tableInputs.add("f2d");
-		tableInputs.add("ki2llChelseaClinton");
-		tableInputs.add("112222-3390");
-		test.insertIntoTableWithPrimaryKey(table, tableInputs); 
-		
-		//Testing Insertions with primary key
-		test = new DBQuery(con);
-		tableInputs = new ArrayList<Object>();
-		tableInputs.add("BernieSanders");
-		tableInputs.add("Garfield_l'zanya");
-		tableInputs.add("112222-3");
-		test.insertIntoTableWithOutPrimaryKey(table, tableInputs); 
-		*/
-		
-		
-		/*
-		//Testing new way of describing tables
-		test.getColumnsOfTable_ArrStr(table);
-		String format = "getColumnsOfTable_ArrStr: \t%s\n"
-					  + "getColumnsOfTable_ArrObj: \t%s\n"
-					  + "getDatatypesOfTable_ArrStr \t%s\n"
-				  	  + "getDatatypesOfTable_ArrObj \t%s\n"
-					  + "getColumnsAndDatatypeFromTable_2DArrObj \t%s\n"
-					  + "getColumnsAndDatatypeFromTable_2DArrStr \t\n%s\n"
-					  ;
-		String s = String.format(
-						format, 
-						test.getColumnsOfTable_ArrStr(table),
-						test.getColumnsOfTable_ArrObj(table),
-						test.getDatatypesOfTable_ArrStr(table),
-						test.getDatatypesOfTable_ArrObj(table),
-						test.getColumnsAndDatatypeFromTable_2DArrObj(table),
-						test.getColumnsAndDatatypeFromTable_2DArrStr(table)
-					);
-		System.out.println(s);		
-		*/
-		
-		
-		//Calling whole table
-		/*
-		System.out.println("=".repeat(40));
-		System.out.println("test.getFromTable_2DArrStr(table)");
+		tableInputs.add(9);
+		tableInputs.add("h");
+		tableInputs.add("h");
+		tableInputs.add("h");
+		test.insertIntoTableWithPrimaryKey(table, tableInputs);
 		System.out.println(test.getFromTable_2DArrStr(table));
-		System.out.println("-".repeat(40));
-		*/
-		
-		//Calling columns of a table
-		/*
-		ArrayList<String> columns = new ArrayList<String>();
-		columns.add("user_id");
-		columns.add("user_email");
-		System.out.println("~".repeat(75));
-		System.out.println("test.getFromTable_2DArrStr(table, columns)");
-		System.out.println(test.getFromTable_2DArrStr(table, columns));
-		System.out.println("~".repeat(75));
-		*/
-		
-		
-		//Calling Where of a table
-		/*
-		ArrayList<String> wheres = new ArrayList<String>();
-		wheres.add("user_email");
-		ArrayList<String> wheresValues = new ArrayList<String>();
-		wheresValues.add("a");
-		System.out.println(test.getFromTable_2DArrStr(table, wheres, wheresValues));
-		*/
-		
-		//Calling Columns and Wheres of a table
-		/*
-		ArrayList<String> columns = new ArrayList<String>();
-		columns.add("user_id");
-		columns.add("user_password");
-		ArrayList<String> wheres = new ArrayList<String>();
-		wheres.add("user_email");
-		ArrayList<String> wheresValues = new ArrayList<String>();
-		wheresValues.add("a");
-		System.out.println("=".repeat(10));
-		System.out.println("test.getFromTable_2DArrStr(table, columns, wheres, wheresValues)");
-		System.out.println(test.getFromTable_2DArrStr(table, columns, wheres, wheresValues));
-		System.out.println("-".repeat(10));
-		*/
 	}
 }

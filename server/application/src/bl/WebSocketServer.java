f304beafc5122333169aa03958f8d8d435c916b8
roodlandy <78751319+roodlandy@users.noreply.github.com>
2023-05-01 21:16:37 -0400

    moved client

--- server/application/src/bl/WebSocketServer.java
+++ server/application/src/bl/WebSocketServer.java
@@ -11,7 +11,7 @@ public class WebSocketServer {
     public static void main(String[] args) throws Exception
     {
         WebSocketServer server = new WebSocketServer();
-        server.setPort(8080);
+        server.setPort(8090);
         server.start();
         server.join();
     }
--- server/client/gui/script.js
+++
@@ -1,3 +0,0 @@
-function open(content, element) {
-    console.log("Hello World!")
-}

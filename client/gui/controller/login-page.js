70e9b195c82fe812953e3c129f65805638d0a26d
roodlandy <78751319+roodlandy@users.noreply.github.com>
2023-05-01 21:19:58 -0400

    client is in main

---
+++ client/gui/controller/login-page.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    if (username === "user" && password === "pass") {fl
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+window.onload = function() {
+    alert("You started up");
+    ws = new WebSocket("ws://localhost:8090/events/");
+}

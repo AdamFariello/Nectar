63deb4855a40dcea44a7d99909c5799850e9928c
roodlandy <78751319+roodlandy@users.noreply.github.com>
2023-05-04 01:43:45 -0400

    client side of json socket

--- client/gui/controller/login-page.js
+++ client/gui/controller/login-page.js
@@ -1,23 +1,31 @@
+ws = new WebSocket("ws://localhost:8994/nectar/");
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    ws.send("attempt login")
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    if (username === "user" && password === "pass") {
+    /*if (username === "user" && password === "pass") {
         ws.send("Logged in: user pass");
         alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
-    }
+    }*/
+    var loginInfo = {Username:username, Password:password}
+    var request = {message:"login", data:loginInfo}
+    ws.send(JSON.stringify(request));
 })
 
-window.onload = function() {
-    ws = new WebSocket("ws://localhost:8080/nectar/");
+ws.onopen = function(){
+    alert("Connection Established");
+}
+
+ws.onmessage = function(event){
+    alert(event.data);
 }
 
 window.onbeforeunload = function(){

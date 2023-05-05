63d82e50cf3cc5de6c996fdd72ab5939a2fa2287
Fiaz Mushfeq <fiazmushfeq@live.com>
2023-05-05 15:46:30 -0400

    reading test cases and producing objects

--- test/info.js
+++ test/info.js
@@ -1,4 +1,13 @@
 function createObj(username, password) {
-    this.username = username;
-    this.password = password;
+    this.username = username
+    this.password = password
 }
+
+const fs = require("fs")
+fs.readFile("cases.txt", (error, data) => {
+    if(error)
+        throw error
+    console.log(data.toString())
+})
+
+console.log("list of objects:")
